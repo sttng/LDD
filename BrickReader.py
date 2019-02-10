0c83bf405a0662bcc0168add42ad50bd8ad0d9a0
sttng <46765778+sttng@users.noreply.github.com>
2019-02-10 19:22:35 +0800

    Update BrickReader.py

--- BrickReader.py
+++ BrickReader.py
@@ -78,7 +78,7 @@
 					indices_list.append(index)
 						
 				geometry_file_dict["vertices"] = vertices_list
-								geometry_file_dict["vertex_count"] = vertex_count
+				geometry_file_dict["vertex_count"] = vertex_count
 				geometry_file_dict["normals"] = normals_list
 				geometry_file_dict["tex_coords"] = tex_coords_list
 				geometry_file_dict["uv_texture_coords_enabled"] = true
@@ -108,7 +108,7 @@
 				geometry_file_dict["partnumber"] = partnumber
 				
 			else:
-				print 'unknown options: ' + options
+				print 'Unknown Options: ' + options
 				
 			return geometry_file_dict
 								
@@ -126,42 +126,40 @@
 	partnumber = geometry_file_dict_list[0]["partnumber"]
 	
 	with open(partnumber + '.obj', 'w') as file_writer:
-	file_writer.write('o brick_' + partnumber + '\n')
+		file_writer.write('o brick_' + partnumber + '\n')
 	
-	for geometry_file_dict in geometry_file_dict_list:
+		for geometry_file_dict in geometry_file_dict_list:
 		
-		file_writer.write('g ' + fragment + '\n')
+			file_writer.write('g ' + fragment + '\n')
 		
-		for i in range(0, len(geometry_file_dict["vertices"]), 3):
-					file_writer.write('v ' + str(geometry_file_dict["vertices"][i]) + ' ' + str(geometry_file_dict["vertices"][i + 1]) + ' ' + str(geometry_file_dict["vertices"][i + 2]) + ' ' + '\n\n')
+			for i in range(0, len(geometry_file_dict["vertices"]), 3):
+				file_writer.write('v ' + str(geometry_file_dict["vertices"][i]) + ' ' + str(geometry_file_dict["vertices"][i + 1]) + ' ' + str(geometry_file_dict["vertices"][i + 2]) + ' ' + '\n\n')
 	
-	for i in range(0, len(geometry_file_dict["normals"]), 3):
-					file_writer.write('vn ' + str(geometry_file_dict["normals"][i]) + ' ' + str(geometry_file_dict["normals"][i + 1]) + ' ' + str(geometry_file_dict["normals"][i + 2]) + ' ' + '\n\n')
+			for i in range(0, len(geometry_file_dict["normals"]), 3):
+				file_writer.write('vn ' + str(geometry_file_dict["normals"][i]) + ' ' + str(geometry_file_dict["normals"][i + 1]) + ' ' + str(geometry_file_dict["normals"][i + 2]) + ' ' + '\n\n')
 	
-	if (geometry_file_dict["uv_texture_coords_enabled"] == true):
-	
-		for i in range(0, len(geometry_file_dict["tex_coords"]), 2):
-		
-			file_writer.write('vt ' + str(geometry_file_dict["tex_coords"][i]) + ' ' + str(geometry_file_dict["tex_coords"][i + 1]) + ' ' + '\n\n')
+				if (geometry_file_dict["uv_texture_coords_enabled"] == true):
+					for i in range(0, len(geometry_file_dict["tex_coords"]), 2):
+						file_writer.write('vt ' + str(geometry_file_dict["tex_coords"][i]) + ' ' + str(geometry_file_dict["tex_coords"][i + 1]) + ' ' + '\n\n')
 			
-			for i in range(0, len(geometry_file_dict["indices"]), 3):
-					index0 = geometry_file_dict["indices"][i + 0] + offset
-					index1 = geometry_file_dict["indices"][i + 1] + offset
-					index2 = geometry_file_dict["indices"][i + 2] + offset
-					index3 = geometry_file_dict["indices"][i + 0] + uv_offset
-					index4 = geometry_file_dict["indices"][i + 1] + uv_offset
-					index5 = geometry_file_dict["indices"][i + 2] + uv_offset
+					for i in range(0, len(geometry_file_dict["indices"]), 3):
+						index0 = geometry_file_dict["indices"][i + 0] + offset
+						index1 = geometry_file_dict["indices"][i + 1] + offset
+						index2 = geometry_file_dict["indices"][i + 2] + offset
+						index3 = geometry_file_dict["indices"][i + 0] + uv_offset
+						index4 = geometry_file_dict["indices"][i + 1] + uv_offset
+						index5 = geometry_file_dict["indices"][i + 2] + uv_offset
 				
-					file_writer.write('f ' + str(index0) + '/' + str(index3) + '/' + str(index0) + ' ' + str(index1) + "/" + str(index4) + '/' + str(index1) + ' ' + str(index2) + "/" + str(index5) + '/' + str(index2) + '\n')
+						file_writer.write('f ' + str(index0) + '/' + str(index3) + '/' + str(index0) + ' ' + str(index1) + "/" + str(index4) + '/' + str(index1) + ' ' + str(index2) + "/" + str(index5) + '/' + str(index2) + '\n')
 	
-	elif:
+				elif:
 	
-		for i in range(0, len(geometry_file_dict["indices"]), 3):
-					index1 = geometry_file_dict["indices"][i + 0] + offset
-					index2 = geometry_file_dict["indices"][i + 1] + offset
-					index3 = geometry_file_dict["indices"][i + 2] + offset
+					for i in range(0, len(geometry_file_dict["indices"]), 3):
+						index1 = geometry_file_dict["indices"][i + 0] + offset
+						index2 = geometry_file_dict["indices"][i + 1] + offset
+						index3 = geometry_file_dict["indices"][i + 2] + offset
 				
-					file_writer.write('f ' + str(index1) + '//' + str(index1) + ' ' + str(index2) + '//' + str(index2) + " " + str(index3) + '//' + str(index3) + '\n')
+						file_writer.write('f ' + str(index1) + '//' + str(index1) + ' ' + str(index2) + '//' + str(index2) + " " + str(index3) + '//' + str(index3) + '\n')
 	
 	fragment ++
 	offset += geometry_file_dict["vertex_count"]
