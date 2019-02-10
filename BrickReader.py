f0f987c1c458089b364d742abb21410e12536e31
sttng <46765778+sttng@users.noreply.github.com>
2019-02-10 17:29:15 +0800

    Update BrickReader.py

--- BrickReader.py
+++ BrickReader.py
@@ -13,16 +13,21 @@
 
 	files_to_convert = []
 	files_to_convert = glob.glob(os.path.join(path_to_lif_tmp_dir + '/db/Primitives/LOD0', partnumber + '.g*'))
+	geometry_file_dict_list = []
+	
 	
 	#various similar extensions like g, .g1, .g2, …, .g8 exist if the brick is composed of multiple parts. The .g file is the 'base_brick'
 	for geometry_file in files_to_convert:
 		
 		if (os.path.splitext(files_to_convert)[1] == ".g"):
 			base_brick = BrickReader.load_single_geometry_file(geometry_file)
+			geometry_file_dict_list.append(base_brick)
 		else:
 			additional_primitive = BrickReader.load_single_geometry_file(geometry_file)
+			geometry_file_dict_list.append(additional_primitive)
 			
-		base_brick = BrickReader.merge(base_brick, additional_primitive)
+		# base_brick = BrickReader.merge(base_brick, additional_primitive)
+		
 
 	return base_brick
 	
@@ -95,6 +100,7 @@
 					indices_list.append(index)
 				
 				geometry_file_dict["vertices"] = vertices_list
+				geometry_file_dict["vertex_count"] = vertex_count
 				geometry_file_dict["normals"] = normals_list
 				geometry_file_dict["uv_texture_coords_enabled"] = false
 				geometry_file_dict["indices"] = indices_list
@@ -108,6 +114,70 @@
 		except IOError as e:
 			print('\tERROR: Failed to read .g file.')
 			return False
- 
+
+
+@staticmethod
+	def export_to_obj(geometry_file_dict_list):
+	
+	fragment = 0
+	offset = 1
+	uv_offset = 1
+	partnumber = geometry_file_dict_list[0]["partnumber"]
+	
+	with open(partnumber + '.obj', 'w') as file_writer:
+	file_writer.write('o brick_' + partnumber + '\n')
+	
+	for geometry_file_dict in geometry_file_dict_list:
+		
+		file_writer.write('g ' + fragment + '\n')
+		
+		for i in range(0, len(geometry_file_dict["vertices"]), 3):
+					file_writer.write('v ' + str(geometry_file_dict["vertices"][i]) + ' ' + str(geometry_file_dict["vertices"][i + 1]) + ' ' + str(geometry_file_dict["vertices"][i + 2]) + ' ' + '\n\n')
+	
+	for i in range(0, len(geometry_file_dict["normals"]), 3):
+					file_writer.write('vn ' + str(geometry_file_dict["normals"][i]) + ' ' + str(geometry_file_dict["normals"][i + 1]) + ' ' + str(geometry_file_dict["normals"][i + 2]) + ' ' + '\n\n')
+	
+	if (geometry_file_dict["uv_texture_coords_enabled"] == true):
+	
+		for i in range(0, len(geometry_file_dict["tex_coords"]), 2):
+	
+	
+	
+	elif:
+	
+	
+	
+	fragment ++
+		offset += geometry_file_dict["vertex_count"]
+		
+		if (geometry_file_dict["uv_texture_coords_enabled"] == true):
+			uvOffset += geometry_file_dict["vertex_count"]
+		
+	
+	
+	
+	
+	if (geometry_file_dict["uv_texture_coords_enabled"] == false):
+		
+			
+				
+				
+				
+			
+				
+			
+				for i in range(0, len(geometry_file_dict["indices"]), 3):
+					index1 = geometry_file_dict["indices"][i + 0] + 1
+					index2 = geometry_file_dict["indices"][i + 1] + 1
+					index3 = geometry_file_dict["indices"][i + 2] + 1
+				
+					file_writer.write('f ' + str(index1) + '//' + str(index1) + ' ' + str(index2) + '//' + str(index2) + " " + str(index3) + '//' + str(index3) + '\n')
+				
+				file_writer.close()
+			elif (geometry_file_dict["uv_texture_coords_enabled"] == true):
+				
+		return True
+
+
 	
 	
