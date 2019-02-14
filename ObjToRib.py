db8ecea8a386c25b19c7f6a1d0edfef09ab3374e
sttng <46765778+sttng@users.noreply.github.com>
2019-02-14 22:40:57 +0800

    Update ObjToRib.py

--- ObjToRib.py
+++ ObjToRib.py
@@ -29,90 +29,51 @@ import commands
 import shutil
 import array
 
-from subprocess import call
-from os import listdir
-from os.path import isfile, join, splitext
 
-# file information
-template_path = os.getcwd() + '/templates'
-obj_path = template_path + '/obj_files'
-rib_path = template_path + '/rib_files'
+obj_file = sys.argv[1]
 
 
-# get .obj file list
-obj_files_list = [ f for f in listdir(obj_path) if isfile(join(obj_path,f)) and '.obj' in f]
-
-
-# read template file to allow easy adjustment later
-t = open(template_path + '/template.rib')
-temp = t.readlines()
-template = []
-for line in temp:
+f1 = open(obj_file)
+lines = f1.readlines()
+f1lines = []
+for line in lines:
 	if '\n' in line:
 		line = line.split('\n')[0]
-	template.append(line)
-
-for obj_file in obj_files_list:
-	# read and store .obj file
-	f1 = open(obj_path + '/' + obj_file)
-	lines = f1.readlines()
-	f1lines = []
-	for line in lines:
-		if '\n' in line:
-			line = line.split('\n')[0]
-		f1lines.append(line)
-	f1.close()
+	f1lines.append(line)
+f1.close()
 
 
-	# create and append rib file based on template
-	name = splitext(obj_file)[0]
-	f2 = open(rib_path + '/' + name + '.rib', 'w')
-	lineNum = 0
-	for line in template:
-		lineNum += 1
-		if ('RENAME' in line):
-			n = splitext(fileName)[0]
-			f2.write('\t\"' + n + '.jpg\"\n')
-		else:
-			f2.write(line + '\n')
-			if 'WorldBegin' in line:
-				break
+name = obj_file.split(".")
+name = name[0]
+f2 = open(name + '.rib', 'w')
 
 
-	# convert obj file lines to rib compatible format
-	verts = []
-	faces = []
-	for line in f1lines:
-		if 'v ' in line and '.' in line:
-			line = line.rstrip()
-			l = ['%.3f' % float(num) for num in line.split(' ') if 'v' not in num]
-			verts.append(l)
-		if 'f ' in line and '/' in line:
-			l = []
-			for nums in line.split(' '):
-				if 'f' not in nums:
-					l.append(int(nums.split('/')[0]))
-			faces.append(l)
-	
-	# create polygons
-	f2.write('\nAttributeBegin')
-	f2.write('\nAttribute \"identifier\" \"uniform string name\" [\"/root/world/geo/sphere\"]')
-	f2.write('\nTranslate 0 0.098009 -0.204513')
-	f2.write('\nRotate 45 0 0 1')
-	f2.write('\nRotate 145 0 1 0')
-	f2.write('\nRotate 45 1 0 0')
-	f2.write('\nScale 0.3 0.3 0.3')
-	for face in faces:
-		f2.write('\n\tPolygon')
-		newline = ''
-		for i in xrange(0, len(face), 1):
-			for j in xrange(0, len(verts[face[i]-1]), 1):
-				newline += str(verts[face[i]-1][j]) + ' '
-		f2.write("\n\t\t\"P\" [ " + newline + "]")
-	f2.write('\nAttributeEnd\n')
-	
-	
-	# finish and close file
-	for i in range(lineNum, len(template)):
-		f2.write(template[i] + '\n')
-	f2.close()
+# convert obj file lines to rib compatible format
+verts = []
+faces = []
+for line in f1lines:
+	if 'v ' in line and '.' in line:
+		line = line.rstrip()
+		l = ['%.3f' % float(num) for num in line.split(' ') if 'v' not in num]
+		verts.append(l)
+	if 'f ' in line and '/' in line:
+		l = []
+		for nums in line.split(' '):
+			if 'f' not in nums:
+				l.append(int(nums.split('/')[0]))
+		faces.append(l)
+
+# create polygons
+f2.write('# Brick' + name)
+f2.write('\nAttributeBegin')
+f2.write('\nAttribute \"identifier\" \"uniform string name\" [\"' + name + '\"]')
+
+for face in faces:
+	f2.write('\n\tPolygon')
+	newline = ''
+	for i in xrange(0, len(face), 1):
+		for j in xrange(0, len(verts[face[i]-1]), 1):
+			newline += str(verts[face[i]-1][j]) + ' '
+	f2.write("\n\t\t\"P\" [ " + newline + "]")
+f2.write('\nAttributeEnd\n')
+f2.close()
