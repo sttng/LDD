8b1e773083248eb349d7ab454c188fc3e452cb10
sttng <46765778+sttng@users.noreply.github.com>
2019-03-04 08:58:51 +0800

    Update o2r.py

--- o2r.py
+++ o2r.py
@@ -15,62 +15,62 @@ def ObjToRib(obj_file):
 	ip = open(obj_file,'r')
 	#grab the data as lines
 	data = ip.readlines()
-	groups=[]
+	groups= []
 	group = False
-	verts=[]
-	norm=[]
-	text=[]
-	face=[]
+	verts = []
+	norm = []
+	text = []
+	face = []
 	name = obj_file.split(".")
 	name = name[0]
 	# for each line check for one of our tokens
-	for line in data :
+	for line in data:
 		# we assume that our Tokens are always the first element of the line (which IIRC the spec specifies)
 		# so we split each line and look at the first element
 		tokens=line.split()	
 		# make sure we have a token to check against
-		if(len(tokens) >0 ) :
-			if(tokens[0] == "g") :
+		if(len(tokens) >0 ):
+			if(tokens[0] == "g"):
 				#print "found group "
-				if (tokens[1] != group) :
+				if (tokens[1] != group):
 					print 'New Group'
 				group = tokens[1]
 				# then add it to our list
-				groups+=[group]
-			elif(tokens[0] =="v") :
+				groups += [group]
+			elif(tokens[0] == "v"):
 				#print "found vert"
 				# create a tuple of the vertex point values
-				vert=[round(float(tokens[1]),Round),round(float(tokens[2]),Round),round(float(tokens[3]),Round)]
+				vert = [round(float(tokens[1]),Round),round(float(tokens[2]),Round),round(float(tokens[3]),Round)]
 				# then add it to our list
-				verts+=[vert]
-			elif(tokens[0] =="vn") :
+				verts += [vert]
+			elif(tokens[0] == "vn"):
 				#print "found normal"
 				# create a tuple of the normal values
-				normal=[round(float(tokens[1]),Round),round(float(tokens[2]),Round),round(float(tokens[3]),Round)]
+				normal = [round(float(tokens[1]),Round),round(float(tokens[2]),Round),round(float(tokens[3]),Round)]
 				# then add it to our list
-				norm+=[normal]
-			elif(tokens[0] =="vt") :
+				norm += [normal]
+			elif(tokens[0] == "vt"):
 				#print "found texture"
 				# create a tuple of the texture values
 				#*****************************************************************
 				# NOTE RENDERMAN Maps Textures in the T from top to bottom so we
 				# calculate 1.0 - t here so the image will map properly
 				#
-				tx=[round(float(tokens[1]),Round),1-round(float(tokens[2]),Round)]
+				tx = [round(float(tokens[1]),Round),1-round(float(tokens[2]),Round)]
 				#
 				#*****************************************************************
 				# then add it to our list
-				text+=[tx]
+				text += [tx]
 			# now we have a face value
-			elif(tokens[0] =="f") :
+			elif(tokens[0] == "f"):
 				# add the face to the list and we will process it later (see below)
-				face+=[line]
+				face += [line]
 			
 	# close the file
 	ip.close()
 
 	# now we've grabbed all the data we can process each of the faces and write out the rib
-	op =  open(name + '.rib', 'w')
+	op = open(name + '.rib', 'w')
 	
 	op.write('##RenderMan RIB-Structure 1.1 Entity')
 	op.write('\nAttributeBegin #begin Brick ' + name)
@@ -78,32 +78,32 @@ def ObjToRib(obj_file):
 	#"
 	for f in face :
 		# create some empty data structures to be filled as we go
-		vertices=[]
-		normals=[]
-		points=[] 
-		tx=[]
-		fd=f.split() 
+		vertices = []
+		normals = []
+		points = [] 
+		tx = []
+		fd = f.split() 
 		# the face is in the structure shown below Vert / TX / Norm. We are gaurenteed to have a
 		# Vert but the others may not be there
 		#1/1/1 3/2/2 4/3/3 2/4/4
 
-		for perface in fd[1:] :
-			index=perface.split("/")
+		for perface in fd[1:]:
+			index = perface.split("/")
 			# get the point array index
-			pind=int(index[0])-1
+			pind = int(index[0])-1
 			points.append(round(float(verts[pind][0]),Round))
 			points.append(round(float(verts[pind][1]),Round))
 			points.append(round(float(verts[pind][2]),Round))
 			op.write('\n\tPolygon')
 			op.write('\n\t\t\"P\" [' + points + ']')
 			# check for textures and add if there
-			if(index[1] !="") :
+			if(index[1] != ""):
 				tind=int(index[1])-1
 				tx.append(round(float(text[tind][0]),Round))
 				tx.append(round(float(text[tind][1]),Round))
 				op.write('\n\t\t\"facevarying float [2] uv1\" [' + tx + ']')
 			# check for normals and check they are there
-			if(index[2] !="") :
+			if(index[2] != ""):
 				nind=int(index[2])-1
 				normals.append(round(float(norm[nind][0]),Round))
 				normals.append(round(float(norm[nind][1]),Round))
