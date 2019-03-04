381e9722a984982efaea13a747987f4424351729
sttng <46765778+sttng@users.noreply.github.com>
2019-03-04 23:16:04 +0800

    Update o2r.py

--- o2r.py
+++ o2r.py
@@ -17,9 +17,13 @@ def export_obj_to_rib(obj_file):
 	groups = []
 	group = 'no_group'
 	verts = []
+	verts_d = {}
 	norm = []
+	norm_d = {}
 	text = []
+	text_d = {}
 	face = []
+	face_d = {}
 	name = obj_file.split(".")
 	name = name[0]
 	# for each line check for one of our tokens
@@ -30,7 +34,7 @@ def export_obj_to_rib(obj_file):
 		# make sure we have a token to check against
 		if(len(tokens) >0 ):
 			if(tokens[0] == "g"):
-				#print "found group "
+				#print "found group"
 				group = tokens[1]
 				# then add it to our list
 				groups += [group]
@@ -42,7 +46,8 @@ def export_obj_to_rib(obj_file):
 				#
 				vert = [round(float(tokens[1]),Round),round(float(tokens[2]),Round),(-1) * round(float(tokens[3]),Round)]
 				# then add it to our list
-				verts[group] += [vert]
+				verts += [vert]
+				verts_d[group] = verts
 			elif(tokens[0] == "vn"):
 				#print "found normal"
 				# create a tuple of the normal values
@@ -51,7 +56,8 @@ def export_obj_to_rib(obj_file):
 				#
 				normal = [round(float(tokens[1]),Round),round(float(tokens[2]),Round),(-1) *round(float(tokens[3]),Round)]
 				# then add it to our list
-				norm[group] += [normal]
+				norm += [normal]
+				norm_d[group] = norm
 			elif(tokens[0] == "vt"):
 				#print "found texture"
 				# create a tuple of the texture values
@@ -63,11 +69,13 @@ def export_obj_to_rib(obj_file):
 				#
 				#*****************************************************************
 				# then add it to our list
-				text[group] += [tx]
+				text += [tx]
+				text_d[group] = text
 			# now we have a face value
 			elif(tokens[0] == "f"):
 				# add the face to the list and we will process it later (see below)
-				face[group] += [line]
+				face += [line]
+				face_d[group] = face
 			
 	# close the file
 	ip.close()
@@ -77,13 +85,14 @@ def export_obj_to_rib(obj_file):
 	
 	op.write('##RenderMan RIB-Structure 1.1 Entity')
 
-	#" Later this should cover obj. files with no groups also. Currently however the LXF
+	# Later this should cover obj. files with no groups also. Currently however the LXF
 	# (LIF) to OBJ exporter writes groups in any case.
+	print groups
 	for group in groups:
 		op.write('\nAttributeBegin #begin Brick ' + name + '.' + group)
-		op.write('\nAttribute \"identifier\" \"uniform string name\" [\"' + name + '.' + group '\"]')
+		op.write('\nAttribute \"identifier\" \"uniform string name\" [\"' + name + '.' + group + '\"]')
 	
-		for f in face[group]:
+		for f in face_d[group]:
 			# create some empty data structures to be filled as we go
 			vertices = []
 			normals = []
@@ -98,30 +107,30 @@ def export_obj_to_rib(obj_file):
 				index = perface.split("/")
 				# get the point array index
 				pind = int(index[0])-1
-				points.append(round(float(verts[group][pind][0]),Round))
-				points.append(round(float(verts[group][pind][1]),Round))
-				points.append(round(float(verts[group][pind][2]),Round))
+				points.append(round(float(verts_d[group][pind][0]),Round))
+				points.append(round(float(verts_d[group][pind][1]),Round))
+				points.append(round(float(verts_d[group][pind][2]),Round))
 				op.write('\n\tPolygon')
 				points_str = " ".join(map(str, points))
 				op.write('\n\t\t\"P\" [' + points_str + ']')
 				# check for textures and add if there
 				if(index[1] != ""):
 					tind=int(index[1])-1
-					tx.append(round(float(text[group][tind][0]),Round))
-					tx.append(round(float(text[group][tind][1]),Round))
+					tx.append(round(float(text_d[group][tind][0]),Round))
+					tx.append(round(float(text_d[group][tind][1]),Round))
 					tx_str = " ".join(map(str, tx))
 					op.write('\n\t\t\"facevarying float [2] uv1\" [' + tx_str + ']')
 				# check for normals and check they are there
 				if(index[2] != ""):
 					nind=int(index[2])-1
-					normals.append(round(float(norm[group][nind][0]),Round))
-					normals.append(round(float(norm[group][nind][1]),Round))
-					normals.append(round(float(norm[group][nind][2]),Round))
+					normals.append(round(float(norm_d[group][nind][0]),Round))
+					normals.append(round(float(norm_d[group][nind][1]),Round))
+					normals.append(round(float(norm_d[group][nind][2]),Round))
 					normals_str = " ".join(map(str, normals))
 					op.write('\n\t\t\"N\" [' + normals_str + ']')
 					
-		op.write('\nAttributeEnd #end Brick ' + name + '.' + group '\n')
-		op.close()
+		op.write('\nAttributeEnd #end Brick ' + name + '.' + group + '\n')
+	op.close()
 			#"
 			# create a dictionary to store the polygon data, we always have a point so we can add
 			# this directly
