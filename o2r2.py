3f44b843fbb4d50f8d2d504b88de0c8c517a4d29
sttng <46765778+sttng@users.noreply.github.com>
2019-03-08 20:10:47 +0800

    Update o2r2.py

--- o2r2.py
+++ o2r2.py
@@ -4,23 +4,25 @@ import getpass
 import time, random
 import sys
 import argparse
+import collections
 
 obj_file = sys.argv[1]
 
 
-def export_obj_to_rib(File) :
+def export_obj_to_rib(obj_file):
 	Round = 6
-	print "opening : "+File
 	# open the file
-	ip = open(File,'r')
+	ip = open(obj_file,'r')
 	#grab the data as lines
-	data=ip.readlines()
-	verts=[]
-	norm=[]
-	text=[]
-	face=[]
-	face_d= {}
+	data = ip.readlines()
+	verts = []
+	norm = []
+	text = []
+	face = []
+	face_d = {}
 	group = 'no_group'
+	name = obj_file.split(".")
+	name = name[0]
 	# for each line check for one of our tokens
 	for line in data :
 		# we assume that our Tokens are always the first element of the line (which IIRC the rispec specifies)
@@ -34,31 +36,37 @@ def export_obj_to_rib(File) :
 			elif(tokens[0] == 'v'):
 				#print "found vert"
 				# create a tuple of the vertex point values
-				vert=[round(float(tokens[1]),Round),round(float(tokens[2]),Round),round(float(tokens[3]),Round)]
+				#*****************************************************************
+				# NOTE RENDERMAN is left handed coordinate system, obj is right handed -> z-axis inverted
+				#
+				vert = [round(float(tokens[1]), Round),round(float(tokens[2]), Round), (-1) * round(float(tokens[3]), Round)]
 				# then add it to our list
-				verts+=[vert]
-			elif(tokens[0] =="vn") :
+				verts += [vert]
+			elif(tokens[0] == 'vn'):
 				#print "found normal"
 				# create a tuple of the normal values
-				normal=[round(float(tokens[1]),Round),round(float(tokens[2]),Round),round(float(tokens[3]),Round)]
+				#*****************************************************************
+				# NOTE RENDERMAN is left handed coordinate system, obj is right handed -> z-axis inverted
+				#
+				normal = [round(float(tokens[1]), Round),round(float(tokens[2]), Round), (-1) * round(float(tokens[3]), Round)]
 				# then add it to our list
-				norm+=[normal]
-			elif(tokens[0] =="vt") :
+				norm += [normal]
+			elif(tokens[0] == 'vt'):
 				#print "found texture"
 				# create a tuple of the texture values
 				#*****************************************************************
 				# NOTE RENDERMAN Maps Textures in the T from top to bottom so we
 				# calculate 1.0 - t here so the image will map properly
 				#
-				tx=[round(float(tokens[1]),Round),1-round(float(tokens[2]),Round)]
+				tx = [round(float(tokens[1]),Round),1-round(float(tokens[2]),Round)]
 				#
 				#*****************************************************************
 				# then add it to our list
-				text+=[tx]
+				text += [tx]
 			# now we have a face value
-			elif(tokens[0] =="f") :
+			elif(tokens[0] == 'f'):
 				# add the face to the list and we will process it later (see below)
-				face+=[line]
+				face += [line]
 				face_d[group] = face
 			
 	# close the file
@@ -66,25 +74,23 @@ def export_obj_to_rib(File) :
 
 	# now we've grabbed all the data we can process each of the faces and write out the rib
 	# Assume faces are group specific
-	obj_group = {}
-	obj_group['no_group'= {}
-	obj_group['no_group'['faces'] = {}
-	obj_group['no_group']['faces']['VTN'] = {}
+	nested_dict = lambda: collections.defaultdict(nested_dict)
+	obj_group = nested_dict()	
 	
 	for group in face_d.keys():
 		face = face_d[group]
-	
+		i = 0
 		for f in face :
 			# create some empty data structures to be filled as we go
-			vertices=[]
-			normals=[]
-			points=[] 
-			tx=[]
-			fd=f.split() 
+			vertices = []
+			normals = []
+			points = [] 
+			tx = []
+			fd = f.split() 
 			# the face is in the structure shown below Vert / TX / Norm we are gaurenteed to have a
 			# Vert but the others may not be there
 			#1/1/1 3/2/2 4/3/3 2/4/4 
-			i = 0
+			
 			for perface in fd[1:] :
 				index=perface.split("/")
 				# get the point array index
@@ -102,7 +108,7 @@ def export_obj_to_rib(File) :
 					tx_str = ' '.join(map(str, tx))
 					obj_group[group][i]["T"] = tx_str
 				else:
-					obj_group[group][i]["T"] = '' #just to ensure
+					obj_group[group][i]["T"] = '' #just to ensure there is no dict error later
 				# check for normals and check they are there
 				if(index[2] !="") :
 					nind=int(index[2])-1
@@ -112,19 +118,32 @@ def export_obj_to_rib(File) :
 					normals_str = ' '.join(map(str, normals))
 					obj_group[group][i]["N"] = normals_str
 				else:
-					obj_group[group][i]["N"] = ''
+					obj_group[group][i]["N"] = '' #just to ensure no dict error later
 				i += 1
 				
-		# Now the dict should have everything
-		del obj_group['no_group'] 
-		for group in obj_group.keys():
-			for f in obj_group[group].keys():
-				print obj_group[group][f]["P"]
-				print obj_group[group][f]["T"]
-				print obj_group[group][f]["N"]
-			
-		# create a dictionary to store the polygon data, we always have a point so we can add
-		#this directly
+	# Now the dict should have everything
+	
+	op = open(name + '.rib', 'w')
+	
+	op.write('##RenderMan RIB-Structure 1.1 Entity\n')
+	# Later this should cover obj. files with no groups also. Currently however the LXF
+	# (LIF) to OBJ exporter writes groups in any case, so we asume we have.
+	for group in obj_group.keys():
+		op.write('AttributeBegin #begin Brick ' + name + '.' + group + '\n')
+		op.write('Attribute "identifier" "uniform string name" ["Brick ' + name + '.' + group + '"]\n')
+		uv_num = 1
+		for f in obj_group[group].keys():
+			op.write('\tPolygon\n')
+			op.write('\t\t"P" [' + str(obj_group[group][f]["P"]) + ']\n')
+			# check for textures
+			if(str(obj_group[group][f]["T"]) != ''):
+				op.write('\t\t"facevarying float [2] uv' + str(uv_num) + '" [' + str(obj_group[group][f]["T"]) + ']\n')
+				uv_num += 1
+			# check for normals
+			if(str(obj_group[group][f]["N"]) != ''):
+				op.write('\t\t"N" [' + str(obj_group[group][f]["N"]) + ']\n')
+
+		op.write('AttributeEnd #end Brick ' + name + '.' + group + '\n\n')
 
 		
 export_obj_to_rib(obj_file)
