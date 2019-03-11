e9d4d78433acb85bad56de685136cc7f4231aab8
sttng <46765778+sttng@users.noreply.github.com>
2019-03-11 14:44:46 +0800

    Update ObjToRib2.py

--- ObjToRib2.py
+++ ObjToRib2.py
@@ -28,11 +28,10 @@ obj_file = sys.argv[1]
 def export_obj_to_rib(obj_file, material_id_list, decoration_id_list):
 	if (material_id_list):
 		bxdf_mat = MaterialsTexts.material_ids_to_ri(material_id_list)
-		if (decoration_id_list != False):
-			bxdf_mat = MaterialsTexts.decoration_ids_to_ri(material_id_list, decoration_id_list)
-	
-	material_string = '_'.join(material_id_list)
-	
+		material_string = '_'.join(material_id_list)
+		#if (decoration_id_list != False):
+		#	bxdf_mat = MaterialsTexts.decoration_ids_to_ri(material_id_list, decoration_id_list)
+
 	Round = 6
 	# open the file
 	ip = open(obj_file,'r')
@@ -175,7 +174,7 @@ def export_obj_to_rib(obj_file, material_id_list, decoration_id_list):
 			if(str(obj_group[group][f]["N"]) != ''):
 				op.write('\t\t"N" [ ' + str(obj_group[group][f]["N"]) + ' ]\n')
 
-		op.write('AttributeEnd #end Brick ' + name + '.' + group + '\n\n')
+		op.write('AttributeEnd #end Brick ' + name + '_' + material_string + '.' + group + '\n\n')
 
 		
 #export_obj_to_rib(obj_file)
