0d8a0faca94422ba34b8c502e15d06a240139a87
sttng <46765778+sttng@users.noreply.github.com>
2019-04-24 10:36:02 +0800

    Update LegoToR.py

--- LegoToR.py
+++ LegoToR.py
@@ -4,8 +4,28 @@ import sys,os.path,subprocess
 cwd = os.getcwd()
 
 # Main rendering routine
-def main(filename, srate=10, pixelvar=0.1, width=1024, height=720, searcharchive=cwd, searchtexture=cwd, integrator='PxrPathTracer', integratorParams={}):
+def main(filename, srate=10, pixelvar=0.1, width=1280, height=720, searcharchive=cwd, searchtexture=cwd, integrator='PxrPathTracer', integratorParams={}):
 	print ('shading rate {} pivel variance {} using {} {}'.format(shadingrate,pixelvar,integrator,integratorParams))
+	template_rib = '''##RenderMan RIB
+version 3.04
+Option "searchpath" "string archive" ["''' + searcharchive + '''"]
+Option "searchpath" "string texture" [".:@:/Applications/Pixar/RenderManProServer-22.4/lib/RenderManAssetLibrary/EnvironmentMaps/Outdoor/GriffithObservatory.rma:''' + searchtexture + '''/"]
+Option "Ri" "int Frame" [1]
+	"float PixelVariance" [''' + pixelvar +''']
+	"string PixelFilterName" ["gaussian"]
+	"float[2] PixelFilterWidth" [2 2]
+	"int[2] FormatResolution" [''' + width + ''' ''' + height + ''']
+	"float FormatPixelAspectRatio" [1]
+	"float[2] Clipping" [0.1 10000]
+	"float[4] ScreenWindow" [-1 1 -0.5625 0.5625]
+	"float[2] Shutter" [0 0]
+Option "bucket" "string order" ["circle"]
+
+''' + integrator + '''
+Hider "raytrace" "int minsamples" [32] "int maxsamples" [64] "float darkfalloff" [0.025] "int incremental" [1] "string pixelfiltermode" ["importance"]
+ShadingRate ''' + srate + '''
+Projection "PxrCamera" "float fov" [8.5] "float fStop" [3.5] "float focalLength" [0.8] "float focalDistance" [5] "point focus1" [0.0 0.0 -1] "point focus2" [1 0.0 -1] "point focus3" [1 1 -1]
+'''
 
 if __name__ == '__main__':
 	cl.ProcessCommandLine('myscene.rib')
