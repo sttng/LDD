127ef59faf4fdf4ed4c30270e51d2de6bc3f0428
sttng <46765778+sttng@users.noreply.github.com>
2019-04-26 13:34:30 +0800

    Update LegoToR.py

--- LegoToR.py
+++ LegoToR.py
@@ -20,7 +20,7 @@ def main(infile, srate, pixelvar, width, height, fov, searcharchive, searchtextu
 	cwd = os.getcwd()
 	infile = os.path.splitext(os.path.basename(infile))[0]
 	
-	template_rib = '''##RenderMan RIB
+	rib_template = '''##RenderMan RIB
 version 3.04
 Option "searchpath" "string archive" ["''' + str(searcharchive) + '''"] "string texture" [".:@:/Applications/Pixar/RenderManProServer-22.4/lib/RenderManAssetLibrary/EnvironmentMaps/Outdoor/GriffithObservatory.rma:''' + str(searchtexture) + '''/"]
 Option "Ri" "int Frame" [1]
@@ -62,7 +62,10 @@ DisplayChannel "vector backward" "string source" "vector motionBack"
 
 Projection "PxrCamera" "float fov" [''' + str(fov) + '''] "float fStop" [3.5] "float focalLength" [0.8] "float focalDistance" [5] "point focus1" [0.0 0.0 -1] "point focus2" [1 0.0 -1] "point focus3" [1 1 -1]''' + str(useplane)
 
-	print template_rib
+	#print template_rib
+	with open('rib_template.rib', 'w') as file_writer:
+		file_writer.write(rib_template)
+	file_writer.close()
 	
 if __name__ == '__main__':
 	cl.ParseCommandLine('')
