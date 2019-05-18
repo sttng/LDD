d81a4766d2699000afc3a56752d292f44f31f589
sttng <46765778+sttng@users.noreply.github.com>
2019-05-18 19:25:46 +0800

    Update pyldd2obj2.py

--- pyldd2obj2.py
+++ pyldd2obj2.py
@@ -584,35 +584,35 @@ class Converter(object):
 					geo = geometriecache[pa.designID]
 					print("-(" + geo.designID + ") " + geo.Partname)
 				
-				
-				# n11=a, n21=d, n31=g, n41=x,
-				# n12=b, n22=e, n32=h, n42=y,
-				# n13=c, n23=f, n33=i, n43=z,
-				# n14=0, n24=0, n34=0, n44=1
+				if (len(pa.Bones) > 1):
+					# n11=a, n21=d, n31=g, n41=x,
+					# n12=b, n22=e, n32=h, n42=y,
+					# n13=c, n23=f, n33=i, n43=z,
+					# n14=0, n24=0, n34=0, n44=1
 									
-				# Read out 1st Bone matrix values
-				n11 = pa.Bones[0].matrix.n11
-				n12 = pa.Bones[0].matrix.n12
-				n13 = pa.Bones[0].matrix.n13
-				n14 = pa.Bones[0].matrix.n14
-				n21 = pa.Bones[0].matrix.n21
-				n22 = pa.Bones[0].matrix.n22
-				n23 = pa.Bones[0].matrix.n23
-				n24 = pa.Bones[0].matrix.n24
-				n31 = pa.Bones[0].matrix.n31
-				n32 = pa.Bones[0].matrix.n32
-				n33 = pa.Bones[0].matrix.n33
-				n34 = pa.Bones[0].matrix.n34
-				n41 = pa.Bones[0].matrix.n41
-				n42 = pa.Bones[0].matrix.n42
-				n43 = pa.Bones[0].matrix.n43
-				n44 = pa.Bones[0].matrix.n44
+					# Read out 1st Bone matrix values
+					n11 = pa.Bones[0].matrix.n11
+					n12 = pa.Bones[0].matrix.n12
+					n13 = pa.Bones[0].matrix.n13
+					n14 = pa.Bones[0].matrix.n14
+					n21 = pa.Bones[0].matrix.n21
+					n22 = pa.Bones[0].matrix.n22
+					n23 = pa.Bones[0].matrix.n23
+					n24 = pa.Bones[0].matrix.n24
+					n31 = pa.Bones[0].matrix.n31
+					n32 = pa.Bones[0].matrix.n32
+					n33 = pa.Bones[0].matrix.n33
+					n34 = pa.Bones[0].matrix.n34
+					n41 = pa.Bones[0].matrix.n41
+					n42 = pa.Bones[0].matrix.n42
+					n43 = pa.Bones[0].matrix.n43
+					n44 = pa.Bones[0].matrix.n44
 									
-				# Create numpy matrix from them and create inverted matrix
-				x = np.array([[n11,n21,n31,n41],[n12,n22,n32,n42],[n13,n23,n33,n43],[n14,n24,n34,n44]])
-				x_inv = np.linalg.inv(x)
+					# Create numpy matrix from them and create inverted matrix
+					x = np.array([[n11,n21,n31,n41],[n12,n22,n32,n42],[n13,n23,n33,n43],[n14,n24,n34,n44]])
+					x_inv = np.linalg.inv(x)
 									
-				undoTransformMatrix = Matrix3D(n11=x_inv[0][0],n12=x_inv[0][1],n13=x_inv[0][2],n14=x_inv[0][3],n21=x_inv[1][0],n22=x_inv[1][1],n23=x_inv[1][2],n24=x_inv[1][3],n31=x_inv[2][0],n32=x_inv[2][1],n33=x_inv[2][2],n34=x_inv[2][3],n41=x_inv[3][0],n42=x_inv[3][1]=,n43=x_inv[3][2],n44=x_inv[3][3])
+					undoTransformMatrix = Matrix3D(n11=x_inv[0][0],n12=x_inv[0][1],n13=x_inv[0][2],n14=x_inv[0][3],n21=x_inv[1][0],n22=x_inv[1][1],n23=x_inv[1][2],n24=x_inv[1][3],n31=x_inv[2][0],n32=x_inv[2][1],n33=x_inv[2][2],n34=x_inv[2][3],n41=x_inv[3][0],n42=x_inv[3][1]=,n43=x_inv[3][2],n44=x_inv[3][3])
 				
 				out.write("g " + "(" + geo.designID + ") " + geo.Partname + '\n')
 				out2 = open(geo.designID + ".obj", "w+")
