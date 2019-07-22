a101b8fb5af1acd4c7e9f3f26f372010c48c0ef4
sttng <46765778+sttng@users.noreply.github.com>
2019-07-22 20:48:34 +0800

    Update pxrxmlargs2rib.py

--- pxrxmlargs2rib.py
+++ pxrxmlargs2rib.py
@@ -28,52 +28,32 @@ class ArgsFile:
 		self.Name = os.path.splitext(os.path.basename(file.name))[0]
 		i = 0
 		
-		#shaderType = xml.getElementsByTagName("shaderType")[0]
-		#shaderTypeTag = shaderType.getElementsByTagName("tag")[0]
-		#shaderValue = shaderTypeTag.getAttribute("value").capitalize()
-		#print('{0} "{1}" "{1}1"').format(shaderValue, self.Name)
-		#
-		#params = xml.getElementsByTagName("param")
-		#for param in params:
-		#	paramType = param.getAttribute("type")
-		#	paramName = param.getAttribute("name")
-		#	paramDefault = "''"
-		#	if param.hasAttribute("default"):
-		#		paramDefault = param.getAttribute("default")
-		#	paramHelp = param.getElementsByTagName("help")[0]
-		#	print('\t#{0}').format(paramHelp)
-		#	print('\t"{0} {1}" [{2}]').format(paramType, paramName, paramDefault)
-		#
-		#outputs = xml.getElementsByTagName("output")
-		#print '\nOutputs:\n'
-		#for output in outputs:
-		#	outputName = output.getAttribute("name")
-		#	outputTags= output.getElementsByTagName("tag")
-		#	for outputTag in outputTags:
-		#		outputTagValue = outputTag.getAttribute("value")
-		for node in xml.firstChild.childNodes:
-			if node.nodeName == 'shaderType':
-				for childnode in node.childNodes:
-					if childnode.nodeName == 'tag':
-						print('{0} "{1}" "{1}1"').format(childnode.getAttribute("value").capitalize(), self.Name)
-			elif node.nodeName == 'page':
-				for childnode in node.childNodes:
-					if childnode.nodeName == 'param':
-						print('\t"{0} {1}" [{2}]').format(childnode.getAttribute("type"), childnode.getAttribute("name"), childnode.getAttribute("default"))
-			elif node.nodeName == 'param':
-				print('\t"{0} {1}" [{2}]').format(node.getAttribute("type"), node.getAttribute("name"), node.getAttribute("default"))
-			elif node.nodeName == 'output':
-				if i == 0:
-					print '\nOutputs:\n'
-				i += 1
-				outval = ''
-				for childnode in node.childNodes:
-					if childnode.nodeName == 'tags':
-						for subnode in childnode.childNodes:
-							if subnode.nodeName == 'tag':
-								outval = outval + subnode.getAttribute("value") + ' '
-				print('\t{0} {1}').format(outval, node.getAttribute("name"))
-
+		shaderType = xml.getElementsByTagName("shaderType")[0]
+		shaderTypeTag = shaderType.getElementsByTagName("tag")[0]
+		shaderValue = shaderTypeTag.getAttribute("value").capitalize()
+		print('{0} "{1}" "{1}1"').format(shaderValue, self.Name)
+		
+		params = xml.getElementsByTagName("param")
+		for param in params:
+			paramType = param.getAttribute("type")
+			paramName = param.getAttribute("name")
+			paramDefault = "''"
+			if param.hasAttribute("default"):
+				paramDefault = param.getAttribute("default")
+			#paramHelp = param.getElementsByTagName("help")[0].firstChild.nodeValue
+			print('\t#{0}').format(paramHelp)
+			print('\t"{0} {1}" [{2}]').format(paramType, paramName, paramDefault)
+		
+		outputs = xml.getElementsByTagName("output")
+		print '\nOutputs:\n'
+		for output in outputs:
+			outputName = output.getAttribute("name")
+			outputTags= output.getElementsByTagName("tag")
+			outputTagValue = ''
+			for outputTag in outputTags:
+				outputTagValue = outputTagValue + outputTag.getAttribute("value") + ' '
+				#outputTagValue = outputTagValue + ' '
+			print('\t{0} {1}').format(outputTagValue, outputName)
 
 def main():
 	try:
