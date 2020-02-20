9913052368d87ad6b66f8b023a04e38ddff08a5d
sttng <46765778+sttng@users.noreply.github.com>
2020-02-20 15:23:56 +0800

    Update LegoToRHD.py

--- LegoToRHD.py
+++ LegoToRHD.py
@@ -10,7 +10,7 @@
 # Updates:
 # 
 # 0.4.7 added brick seams via scale factor of 0.985 for each brick
-# 0.4.6 Added nonormals switch (-nn), to ignore normals writing as some parts of LDD seem to have incorrect normals.
+# 0.4.6 added nonormals switch (-nn), to ignore normals writing as some parts of LDD seem to have incorrect normals.
 # 0.4.5 remove parts writing of normals for the time being
 # 0.4.4 small optimization, removal of legacy code
 # 0.4.3 added displayColor primvar and other fixes
