aa1531268e40ff4fdc5457f1c3c9df9c36738091
sttng <46765778+sttng@users.noreply.github.com>
2020-06-11 07:41:50 +0800

    Update LIFCreate.py

--- LIFCreate.py
+++ LIFCreate.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python
+
+"""
+    LIF Extractor - LEGO Digital Designer LIF extractor.
+
+    Copyright (C) 2012 JrMasterModelBuilder
+
+    You accept full responsibility for how you use this program.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 import os
 import sys
+import struct
 import time
 
 def create(path):
+	filename = os.path.basename(os.path.normpath(path))
+	binary_file = open((filename + '.lif'), "wb")
+	binary_file.write(b"LIFF")
+	
 	# Set the directory you want to start from
-	rootDir = '.'
+	rootDir = path
 	for dirName, subdirList, fileList in os.walk(rootDir):
 		print('Found directory: %s' % dirName)
 		number_dirs = len(subdirList)
@@ -13,42 +41,155 @@ def create(path):
 		number_entries = number_dirs + number_files
 		print number_entries
 		# Create four bytes from the integer
-		four_bytes = number_entries.to_bytes(4, byteorder='big', signed=True)
+		#four_bytes = number_entries.to_bytes(4, byteorder='big', signed=True)
 		binary_file.write(b'\x00\x01') #Entry type (equals 1)
 		binary_file.write(b'\x00\x00\x00\x00') #Unknown value (equals 0 or 7) The value 0 seems to be used for the root folder
 		binary_file.write(dirName.encode('utf8')) #File name. (Unicode null-terminated text)
 		binary_file.write(b'\x00\x00\x00\x00') #Spacing (Always equals 0)
 		binary_file.write(b'\x00\x00\x00\x00') #Block size (Always equals 20 so it equals the block header size)
-		binary_file.write(four_bytes) #The number of sub-entries (files and folders)
+		binary_file.write(struct.pack('>I', number_entries)) #The number of sub-entries (files and folders)
 		
-			for fname in fileList:
-				print('\t%s' % fname)
-				file_size = os.path.getsize(fname)
+		for fname in fileList:
+			print('\t%s' % fname)
+			file_size = os.path.getsize(fname)
 				
-				print("Last modified: %s" % time.ctime(os.path.getmtime(fname)))
-				print("Created: %s" % time.ctime(os.path.getctime(fname)))
+			print("Last modified: %s" % time.ctime(os.path.getmtime(fname)))
+			#created = time.ctime(os.path.getctime(fname))
+			#print os.stat(fname)[9]
 				
-				binary_file.write(b'\x00\x10') #Entry type (equals 2)
-				binary_file.write(b'\x00\x00\x00\x00') #Spacing/unknown value (0 or 7)
-				binary_file.write(fname.encode('utf8')) #File name. (Unicode null-terminated text)
-				binary_file.write(b'\x00\x00\x00\x00') #Spacing (Always equals 0)
-				binary_file.write(file_size + 20) #File size (it is actually the block size because it includes the block header size (20))
-				binary_file.write(b'\x00\x00\x00\x00\x00\x00\x00\x00') #Created, modified or accessed date
-				binary_file.write(b'\x00\x00\x00\x00\x00\x00\x00\x00') #Created, modified or accessed date
-				binary_file.write(b'\x00\x00\x00\x00\x00\x00\x00\x00') #Created, modified or accessed date
-				
-				
-				
+			binary_file.write(b'\x00\x10') #Entry type (equals 2)
+			binary_file.write(b'\x00\x00\x00\x00') #Spacing/unknown value (0 or 7)
+			binary_file.write(fname.encode('utf8')) #File name. (Unicode null-terminated text)
+			binary_file.write(b'\x00\x00\x00\x00') #Spacing (Always equals 0)
+			binary_file.write(struct.pack('>I', file_size + 20)) #File size (it is actually the block size because it includes the block header size (20))
+			binary_file.write(struct.pack('>Q', os.stat(fname)[9])) #Created, modified or accessed date
+			binary_file.write(b'\x00\x00\x00\x00\x00\x00\x00\x00') #Created, modified or accessed date
+			binary_file.write(b'\x00\x00\x00\x00\x00\x00\x00\x00') #Created, modified or accessed date
+			print('\t%s' % fname)
+	
+	
+	
+	
+	
+
+	binary_file.close()
+
+def extract(path):
+    
+    def uint32(offset):
+        if(bytesAre == "str"):
+            return (ord(fileData[offset]) * 16777216) + (ord(fileData[offset+1]) * 65536) + (ord(fileData[offset+2]) * 256) + ord(fileData[offset+3])
+        else:
+            return (fileData[offset] * 16777216) + (fileData[offset+1] * 65536) + (fileData[offset+2] * 256) + fileData[offset+3]
+    
+    def uint16(offset):
+        if(bytesAre == "str"):
+            return (ord(fileData[offset]) * 256) + ord(fileData[offset+1])
+        else:
+            return (fileData[offset] * 256) + fileData[offset+1]
+    
+    def recurse(prefix, offset):
+        if(prefix == ""):
+            offset += 36
+        else:
+            folderList.extend([prefix])
+            offset += 4
+            
+        for i in range(uint32(offset)):
+            offset += 4
+            entryType = uint16(offset)#1 = directory, 2 = file
+            offset += 6
+            #Build the name.
+            entryName = os.sep
+            #Check both integer and byte for different versions of Python.
+            while(fileData[offset+1] != 0 and fileData[offset+1] != b"\x00"):
+                if(bytesAre == "str"):
+                    entryName += fileData[offset+1]
+                else:
+                    entryName += chr(fileData[offset+1])
+                offset += 2
+            offset += 6
+            
+            if(entryType == 1):
+                #Recurse through the director, and update the offset.
+                packedFilesOffset[0] += 20
+                offset = recurse(prefix + entryName, offset)
+            elif(entryType == 2):
+                #File offset.
+                packedFilesOffset[0] += 20
+                fileOffset = packedFilesOffset[0]
+                #File size.
+                fileSize = uint32(offset) - 20
+                offset += 24
+                packedFilesOffset[0] += fileSize
+                fileList.extend([[prefix + entryName, fileOffset, fileSize]])
+        #Return the offset at the end of this run to update parent runnings.
+        return offset
+    
+    print("PROCESSING: " + path)
+    #Open the file if valid.
+    try:
+        with open(path, "rb") as f:
+            fileData = f.read()
+    except IOError as e:
+        print("\tERROR: Failed to read file.")
+        return False
+    
+    if(len(fileData) < 4 or fileData[0:4] != b"LIFF"):
+        print("\tERROR: Not a LIF file.")
+        return False
+    
+    print("\tEXTRACTING: Please wait.")
+    
+    #Recurse through, creating a list of all the files.
+    packedFilesOffset = [84]#Array for non-global function-modifiable variable.
+    folderList = []
+    fileList = []
+    #Check if this version of Python treats bytes as int or str
+    bytesAre = type(b'a'[0]).__name__    
+    #Recuse through the archive.
+    recurse("", (uint32(72)+64))
+    
+    #Create output path from input path.
+    if(path[-4:].lower() == ".lif"):
+        outFolder = path[:-4]
+    else:
+        outFolder = path
+    #Create the first non-existant folder to extract to.
+    if os.path.exists(outFolder):
+        i = 1
+        while(os.path.exists(outFolder + "_" + str(i))):
+            i += 1            
+        outFolder = outFolder + "_" + str(i)
+    
+    #Make the output folder.
+    os.makedirs(outFolder)
+    
+    #Create all the folders we need to extract to.
+    for a in folderList:
+        if(a[0] == ""):
+            continue
+        if not os.path.exists(outFolder + a):
+            os.makedirs(outFolder + a)
+    
+    #Loop through the list of files, saving them.
+    for a in fileList:
+        f = open((outFolder + a[0]), "wb")
+        f.write(fileData[a[1]:a[1]+a[2]])
+        f.close()
+    
+    print("\tCOMPLETE: " + str(len(fileList)) + " files in " + str(len(folderList)) + " folders extracted.")
+    return True
 
 #Detect if executable or not.
 fileName = sys.argv[0].split(os.sep).pop()
 if(fileName[-3:] == ".py" or fileName[-4:] == ".pyw"):
-	runCommand = "python " + fileName
+    runCommand = "python " + fileName
 else:
-	runCommand = fileName
+    runCommand = fileName
 
 if(len(sys.argv) > 1):
-	for i in range(1, len(sys.argv)):
-		create(sys.argv[i])
+    for i in range(1, len(sys.argv)):
+        create(sys.argv[i])
 else:
-	print("LIF Creator 1.0\n\nThis program will create LIF archives from a folder.\n\nCOPYRIGHT:\n\t(C) 2020 sttng\n\nLICENSE:\n\tGNU GPLv3\n\tYou accept full responsibility for how you use this program.\n\nUSEAGE:\n\t" + runCommand + " <FILE_PATH>")
+    print("LIF Creator 1.0\n\nThis program will create LIF archives from an adjacent folder.\n\nCOPYRIGHT:\n\t(C) 2012 JrMasterModelBuilder\n\nLICENSE:\n\tGNU GPLv3\n\tYou accept full responsibility for how you use this program.\n\nUSEAGE:\n\t" + runCommand + " <FILE_PATHS>")
