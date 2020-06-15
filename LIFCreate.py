ed9ca4c1765915e31650bd1750fd8ed15ffd1763
sttng <46765778+sttng@users.noreply.github.com>
2020-06-15 16:15:09 +0800

    Update LIFCreate.py

--- LIFCreate.py
+++ LIFCreate.py
@@ -19,6 +19,7 @@
 
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	https://stackoverflow.com/questions/2212643/python-recursive-folder-read
 """
 
 
