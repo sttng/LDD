a00dd632b5193007daaef20754686e49484245ad
sttng <46765778+sttng@users.noreply.github.com>
2020-06-16 10:26:41 +0800

    Update LIFCreate.py

--- LIFCreate.py
+++ LIFCreate.py
@@ -104,6 +104,39 @@ class LIFBlock:
 		return out
 
 
+
+def walkDir(walk_dir):
+	# If your current working directory may change during script execution, it's recommended to
+	# immediately convert program arguments to an absolute path. Then the variable root below will
+	# be an absolute path as well. Example:
+	# walk_dir = os.path.abspath(walk_dir)
+	print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+
+	for root, subdirs, files in os.walk(walk_dir):
+		print('--\nroot = ' + root)
+		list_file_path = os.path.join(root, 'my-directory-list.txt')
+		print('list_file_path = ' + list_file_path)
+
+		with open(list_file_path, 'wb') as list_file:
+			for subdir in subdirs:
+				print('\t- subdirectory ' + subdir)
+
+			for filename in files:
+				file_path = os.path.join(root, filename)
+				file_size = os.path.getsize(file_path)
+				file_header_size = file_size + 20
+
+				print('\t- file %s (full path: %s)' % (filename, file_path))
+
+				with open(file_path, 'rb') as f:
+					f_content = f.read()
+					list_file.write(('File Header Size: %s \n' % file_header_size).encode('utf-8'))
+					list_file.write(('File %s contents:\n' % filename).encode('utf-8'))
+					list_file.write(f_content)
+					list_file.write(b'\n')
+
+
+
 def create(path):
 	filename = os.path.basename(os.path.normpath(path))
 	test_file = open((filename + '.lif'), "wb")
